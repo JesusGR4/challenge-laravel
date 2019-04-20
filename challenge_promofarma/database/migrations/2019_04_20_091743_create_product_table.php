9bff1c1b19f7b8b28fb29e0a0e7ff54e906bd430
jesgarrio <Pasaje10>
2019-04-21 00:31:06 +0200

    Added status column to Product

--- challenge_promofarma/database/migrations/2019_04_20_091743_create_product_table.php
+++ challenge_promofarma/database/migrations/2019_04_20_091743_create_product_table.php
@@ -16,6 +16,7 @@ class CreateProductTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id_product');
             $table->string('name');
+            $table->boolean('status');
             $table->timestamps();
         });
     }

f877b6a889237be314962af611f81b938129e7e2
jesgarrio <Pasaje10>
2019-04-20 12:36:43 +0200

    Added status attribute to Users

--- challenge_promofarma/database/migrations/2014_10_12_000000_create_users_table.php
+++ challenge_promofarma/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id_user');
             $table->string('name');
+            $table->boolean('status');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
